2f9aeee8f2fd4fc2e3035ad13fe761ae44f6cd89
Rémi Moustey <remimoustey@gmail.com>
2020-03-06 11:23:21 +0100

    All files of the Wordpress site

---
+++ wp-content/themes/child-customizr/templates/parts/content/sidebars/right_sidebar.php
@@ -0,0 +1,48 @@
+<div id="czr_grid-5d133e3a17e5a" class="col-md-12 grid-container grid-container__classic tc-grid-shadow tc-grid-border tc-grid-hover-move" data-czr-model_id="post_list_grid" data-czr-template="templates/parts/modules/grid/grid_wrapper">
+  <div class="grid__wrapper grid">
+    <section class="row grid__section cols-3 grid-section-not-featured">
+      <article id="post-94" class="animated_bloc grid-item col-12 col-md-6 col-xl-4 col-lg-4 post-94 post type-post status-publish format-standard has-post-thumbnail category-actualites czr-hentry" data-czr-model_id="grid_item" data-czr-template="templates/parts/modules/grid/grid_item">
+        <section class="grid__item">
+          <div class="tc-grid-figure entry-media__holder has-thumb czr__r-wGR js-centering" data-img-centered-in-container="1">
+            <div class="entry-media__wrapper czr__r-i">
+              <a class="bg-link" href="http://villedenoumea.com/une-ville-jumelee-pour-noumea"></a>
+              <img width="570" height="350" src="http://villedenoumea.com/wp-content/uploads/2019/06/20ac26df-5807-4787-9c5d-f1b0a9f9b63d-570x350.jpg" class="attachment-tc-grid tc-thumb-type-thumb czr-img wp-post-image h-centered opacity-forced" alt="Comité de jumelage entre Nouméa et Papeete" data-img-centered="2" style="height: 216px; width: auto; left: -1px; top: 0px;">
+            </div>
+              <div class="tc-grid-caption">
+                <div class="entry-summary ">
+                  <div class="tc-g-cont czr-talign"><p>Le 3 juin 2019, un accord a été signé entre les maires de Nouméa et de Papeete, capitale de la Polynésie française, afin d'officialiser le jumelage entre les deux communes.</p></div>
+                </div>
+              </div>
+          </div>
+          <div class="tc-content">
+            <header class="entry-header " data-czr-model_id="post_list_item_header" data-czr-template="templates/parts/content/post-lists/item-parts/headings/post_list_item_header">
+              <div class="entry-header-inner ">
+                <div class="tax__container post-info entry-meta">
+                  <a class="tax__link" href="http://villedenoumea.com/category/actualites" title="Voir tous les billets dans Actualités"> <span>Actualités</span> </a>        
+                </div>
+                <h2 class="entry-title">
+                  <a class="czr-title" href="http://villedenoumea.com/une-ville-jumelee-pour-noumea" rel="bookmark">Papeete jumelée pour Nouméa</a>
+                </h2>
+                <a class="btn btn-edit" title="Modifier" href="http://villedenoumea.com/wp-admin/post.php?post=94&amp;action=edit" target="" rel="nofollow" style=""><i class="icn-edit"></i>Modifier</a>  
+              </div>
+            </header>      
+              <footer class="entry-footer" data-czr-model_id="post_list_item_footer" data-czr-template="templates/parts/content/post-lists/item-parts/footers/post_list_item_footer">     
+                <div class="post-info clearfix entry-meta">
+                  <div class="row flex-row">
+                    <div class="col col-auto"><span class="author-meta">par <span class="author vcard"><span class="author_name"><a class="url fn n" href="http://villedenoumea.com/author/admin-63" title="Voir tous les billets de Ville de Nouméa" rel="author">Ville de Nouméa</a></span></span></span>
+                    </div>
+                    <div class="col col-auto">
+                      <div class="row">
+                        <div class="col col-auto">Publié <a href="http://villedenoumea.com/une-ville-jumelee-pour-noumea" title="Permalien vers &nbsp;Papeete jumelée pour Nouméa" rel="bookmark"><time class="entry-date published updated" datetime="2019-06-27T08:23:26+00:00">juin 27, 2019</time></a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </footer>    
+          </div>
+        </section>
+      </article>
+    </section>
+  </div>
+</div>
